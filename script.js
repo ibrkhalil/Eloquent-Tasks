fd22c10d8d3d3ef27813ad9d7dd5646654e8fc19
vampirekiddo <vampirekiddo@users.noreply.github.com>
2021-03-14 18:06:19 +0200

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+hashCount = "";
+for(let i = 0; i<8; ++i){
+    for(let j = 0; j<i; ++j)
+        {
+            hashCount+='#';
+        }
+    console.log(hashCount+"\n");
+    hashCount = "";
+}
+// task 1
