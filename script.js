e7cf766af0687e9481c92fcde48678b58877d410
vampirekiddo <vampirekiddo@users.noreply.github.com>
2021-03-14 18:33:12 +0200

    First

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-hashCount = "";
-for(let i = 0; i<8; ++i){
-    for(let j = 0; j<i; ++j)
-        {
-            hashCount+='#';
-        }
-    console.log(hashCount+"\n");
-    hashCount = "";
-}
+// hashCount = "";
+// for(let i = 0; i<8; ++i){
+//     for(let j = 0; j<i; ++j)
+//         {
+//             hashCount+='#';
+//         }
+//     console.log(hashCount+"\n");
+//     hashCount = "";
+// }
 // task 1
+
+
+// for(let i = 1; i<=100; ++i){
+//     if(i % 5 == 0 && i % 3 == 0)
+//         console.log('fizzbuzz');
+//     else if(i % 3 == 0)
+//         console.log('fizz');
+//     else if(i % 5 == 0)
+//         console.log('buzz');
+//     else
+//     console.log(i);
+// }
+// task 2
+
+// let gridRow = '';
+// let gridN = 8;
+// for(let i = 0; i<gridN; ++i)
+// {
+//     if(i % 2 == 0)
+//     {
+//       for(let j = 0; j < gridN/2; j++)
+//         gridRow+=' #';
+//         console.log(gridRow,'\n');
+//         gridRow='';}
+//     else
+//     {
+//       for(let j = 0; j < gridN/2; j++)
+//         gridRow+='# ';
+//         console.log(gridRow,'\n');
+//         gridRow='';}
+// }
+// task 3
